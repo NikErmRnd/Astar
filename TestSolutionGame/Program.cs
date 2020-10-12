9bb91839a2beb47f25fc6c2854dbfc455c7ef2e3
Nikolay Ermoshkin <nermoshkin@usetech.ru>
2020-10-12 16:28:56 +0300

    Update cleanup

--- TestSolutionGame/Program.cs
+++ TestSolutionGame/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Threading;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
 
 namespace TestSolutionGame
 {
@@ -14,190 +16,177 @@ namespace TestSolutionGame
 
         static void Main(string[] args)
         {
-            //Console.WriteLine("Hello World!");
+            var lines = File.ReadLines(args[0]).ToList();
+            StringBuilder outSrtBuilder = new StringBuilder();
+            int n = lines.First().Count();
+            int m = lines.Count;
+            var board = new int[n, m];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    if (lines[i][j] == ' ')
+                    {
+                        board[i, j] = 1;
+                    }
+                }
+            }
 
-            var board = new Board();
+            int lootCount = int.Parse(args[2]);
+            var rand = new Random();
+            outSrtBuilder.Append("Lootboxes: ");
+            int playerX = rand.Next(0, n);
+            int playerY = rand.Next(0, m);
+            board[playerX, playerY] = 3;
+            for (int i = 0; i < lootCount; i++)
+            {
+                int x = rand.Next(0, n);
+                int y = rand.Next(0, m);
+                if (board[x, y] != 1)
+                {
+                    board[x, y] = 2;
+                    outSrtBuilder.Append($"({x},{y}) ");
+                }
+                else
+                {
+                    i--;
+                }
+            }
+            outSrtBuilder.Append($"\nPlayer: ({playerX},{playerY})");
 
-            board.board = new int[6, 6];
-            board.board[0, 0] = 0;
-            board.board[0, 1] = 0;
-            board.board[0, 2] = 0;
-            board.board[0, 3] = 1;
-            board.board[0, 4] = 1;
-            board.board[0, 5] = 2;
+            (int, int) lootPoint = (-1, -1);
+            lootPoint = Scan(board, lootPoint, n, m);
+            var startPlayer = new Cell() { X = playerX, Y = playerY, IsStartPoint = true };
+            var newPoint = NexPoint(board, lootPoint, startPlayer, n, m);
+            while (true)
+            {
+                newPoint = NexPoint(board, lootPoint, newPoint, n, m);
+                if (newPoint.X == lootPoint.Item1 && newPoint.Y == lootPoint.Item2)
+                {
+                    board[lootPoint.Item1, lootPoint.Item2] = 0;
+                    lootPoint = (-1, -1);
+                    lootPoint = Scan(board, lootPoint, n, m);
+                    if (lootPoint.Item1 == -1 && lootPoint.Item2 == -1)
+                    {
+                        break;
+                    }
+                }
+            }
+            List<Cell> path = new List<Cell>();
+            path.Add(newPoint);
+            while (newPoint.Prev != null)
+            {
+                newPoint = newPoint.Prev;
+                path.Add(newPoint);
+            }
 
-            board.board[1, 0] = 0;
-            board.board[1, 1] = 0;
-            board.board[1, 2] = 2;
-            board.board[1, 3] = 0;
-            board.board[1, 4] = 0;
-            board.board[1, 5] = 0;
+            outSrtBuilder.Append("\nPath: ");
+            path.Reverse();
+            foreach (var point in path)
+            {
+                outSrtBuilder.Append($"({point.X},{point.Y})");
+            }
+            var file = File.Open(args[1], FileMode.Create);
+            file.Close();
+            using (StreamWriter writeFile = new System.IO.StreamWriter(args[1], true))
+            {
+                writeFile.WriteLine(outSrtBuilder.ToString());
+            }
+        }
 
-            board.board[2, 0] = 0;
-            board.board[2, 1] = 1;
-            board.board[2, 2] = 1;
-            board.board[2, 3] = 3;
-            board.board[2, 4] = 0;
-            board.board[2, 5] = 2;
+        private static (int, int) Scan(int[,] board, (int, int) lootPoint, int n, int m)
+        {
+            //Scan
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    if (board[i, j] == 2)
+                    {
+                        lootPoint = (i, j);
+                        break;
+                    }
+                }
+                if (lootPoint.Item1 != -1 && lootPoint.Item2 != -1)
+                {
+                    break;
+                }
+            }
 
-            board.board[3, 0] = 0;
-            board.board[3, 1] = 0;
-            board.board[3, 2] = 0;
-            board.board[3, 3] = 0;
-            board.board[3, 4] = 0;
-            board.board[3, 5] = 0;
+            return lootPoint;
+        }
 
-            board.board[4, 0] = 2;
-            board.board[4, 1] = 0;
-            board.board[4, 2] = 0;
-            board.board[4, 3] = 1;
-            board.board[4, 4] = 1;
-            board.board[4, 5] = 0;
-
-            board.board[5, 0] = 0;
-            board.board[5, 1] = 0;
-            board.board[5, 2] = 2;
-            board.board[5, 3] = 0;
-            board.board[5, 4] = 0;
-            board.board[5, 5] = 0;
-
-            board.SHOW();
-
-            //Console.WriteLine("+++  L");
-            //Console.WriteLine("++L+++");
-            //Console.WriteLine("+  P+L");
-            //Console.WriteLine("++++++");
-            //Console.WriteLine("L++  +");
-            //Console.WriteLine("++L+++");
-
-            int countLoot = board.Count();
-
-            List<(int, int)> cords;
-            cords = board.FindPath(6, 6);
-
-            //Console.WriteLine("Hello World!");
-            Console.ReadKey();
+        private static Cell NexPoint(int[,] board, (int, int) lootPoint, Cell startPlayer, int n, int m)
+        {
+            Cell newPoint = startPlayer;
+            if (startPlayer.IsStartPoint != true)
+            {
+                if (startPlayer.Neighbors.Count != 1 && startPlayer.Neighbors[0].Sum == startPlayer.Neighbors[1].Sum)
+                {
+                    newPoint = startPlayer.Neighbors.OrderBy(x => Guid.NewGuid()).Take(1).First().DeepClone();
+                }
+                else
+                {
+                    newPoint = startPlayer.Neighbors.OrderBy(item => item.Sum).First().DeepClone();
+                }
+                newPoint.Prev = startPlayer;
+            }
+            if (newPoint.X + 1 < n)
+            {
+                newPoint.Neighbors.Add(new Cell() { X = newPoint.X + 1, Y = newPoint.Y, IsBlock = board[newPoint.X + 1, newPoint.Y] == 1, DistanceFromStart = 10 + (newPoint.Prev == null ? 0 : newPoint.Prev.DistanceFromStart) });
+            }
+            if (newPoint.X - 1 != -1)
+            {
+                newPoint.Neighbors.Add(new Cell() { X = newPoint.X - 1, Y = newPoint.Y, IsBlock = board[newPoint.X - 1, newPoint.Y] == 1, DistanceFromStart = 10 + (newPoint.Prev == null ? 0 : newPoint.Prev.DistanceFromStart) });
+            }
+            if (newPoint.Y + 1 < m)
+            {
+                newPoint.Neighbors.Add(new Cell() { X = newPoint.X, Y = newPoint.Y + 1, IsBlock = board[newPoint.X, newPoint.Y + 1] == 1, DistanceFromStart = 10 + (newPoint.Prev == null ? 0 : newPoint.Prev.DistanceFromStart) });
+            }
+            if (newPoint.Y - 1 != -1)
+            {
+                newPoint.Neighbors.Add(new Cell() { X = newPoint.X, Y = newPoint.Y - 1, IsBlock = board[newPoint.X, newPoint.Y - 1] == 1, DistanceFromStart = 10 + (newPoint.Prev == null ? 0 : newPoint.Prev.DistanceFromStart) });
+            }
+            newPoint.Neighbors = newPoint.Neighbors.Where(item => item.IsBlock != true).ToList();
+            foreach (var cell in newPoint.Neighbors)
+            {
+                cell.EvristDistance = (Math.Abs(lootPoint.Item1 - cell.X) + Math.Abs(lootPoint.Item2 - cell.Y)) * 10;
+                cell.Sum = cell.EvristDistance + cell.DistanceFromStart;
+            }
+            if (startPlayer.IsStartPoint == true)
+            {
+                startPlayer.IsStartPoint = false;
+            }
+            return newPoint;
         }
     }
 
-    public class Board
+    public static class ExtensionMethods
     {
-        public int[,] board;
-
-        internal int Count()
+        // Deep clone
+        public static T DeepClone<T>(this T a)
         {
-            return 5;
-        }
-
-        internal List<(int, int)> FindPath(int n, int m)
-        {
-            List<(int, int)> cords = new List<(int, int)>();
-            cords.Add((2, 3));
-            int score = 0;
-            for (int b = 0; b < 5; b++)
+            using (MemoryStream stream = new MemoryStream())
             {
-                //Move
-                bool finish = false;
-                var rand = new Random();
-                while (finish != true)
-                {
-                    (int, int) currentPoint = cords.Last();
-                    if (board[currentPoint.Item1, currentPoint.Item2] == 2)
-                    {
-                        score++;
-                        board[currentPoint.Item1, currentPoint.Item2] = 0;
-                        break;
-                    }
-
-                    switch (Console.ReadKey().KeyChar)
-                    {
-                        case 'w': { UP(ref currentPoint, cords); Console.Clear(); SHOW(); break; }
-                        case 's': { DOWN(ref currentPoint, cords); Console.Clear(); SHOW(); break; }
-                        case 'a': { LEFT(ref currentPoint, cords); Console.Clear(); SHOW(); break; }
-                        case 'd': { RIGHT(ref currentPoint, cords); Console.Clear(); SHOW(); break; }
-                    }
-                    }
-            }
-            return cords;
-        }
-
-        private bool RIGHT(ref (int, int) currentPoint, List<(int, int)> cords)
-        {
-            if (currentPoint.Item2 + 1 < 6 && board[currentPoint.Item1, currentPoint.Item2 + 1] != 1)
-            {
-                board[currentPoint.Item1, currentPoint.Item2 + 1] = 3;
-                board[currentPoint.Item1, currentPoint.Item2] = 0;
-                currentPoint = (currentPoint.Item1, currentPoint.Item2 + 1);
-                cords.Add(currentPoint);
-                return true;
-            }
-            return false;
-        }
-
-        private bool LEFT(ref (int, int) currentPoint, List<(int, int)> cords)
-        {
-            if (currentPoint.Item2 - 1 != -1 && board[currentPoint.Item1, currentPoint.Item2 - 1] != 1)
-            {
-                board[currentPoint.Item1, currentPoint.Item2 - 1] = 3;
-                board[currentPoint.Item1, currentPoint.Item2] = 0;
-                currentPoint = (currentPoint.Item1, currentPoint.Item2 - 1);
-                cords.Add(currentPoint);
-                return true;
-            }
-            return false;
-        }
-
-        private bool UP(ref (int, int) currentPoint, List<(int, int)> cords)
-        {
-            if (currentPoint.Item1 - 1 != -1 && board[currentPoint.Item1 - 1, currentPoint.Item2] != 1)
-            {
-                board[currentPoint.Item1 - 1, currentPoint.Item2] = 3;
-                board[currentPoint.Item1, currentPoint.Item2] = 0;
-                currentPoint = (currentPoint.Item1 - 1, currentPoint.Item2);
-                cords.Add(currentPoint);
-                return true;
-            }
-            return false;
-        }
-
-        private bool DOWN(ref (int, int) currentPoint, List<(int, int)> cords)
-        {
-            if (currentPoint.Item1 + 1 < 6 && board[currentPoint.Item1 + 1, currentPoint.Item2] != 1)
-            {
-                board[currentPoint.Item1 + 1, currentPoint.Item2] = 3;
-                board[currentPoint.Item1, currentPoint.Item2] = 0;
-                currentPoint = (currentPoint.Item1 + 1, currentPoint.Item2);
-                cords.Add(currentPoint);
-                return true;
-            }
-            return false;
-        }
-
-        internal void SHOW()
-        {
-            for (int i = 0; i<6; i++)
-            {
-                for (int j=0; j<6; j++)
-                {
-                    if (board[i,j] == 3)
-                    {
-                        Console.Write("P");
-                    }
-                    if (board[i, j] == 2)
-                    {
-                        Console.Write("L");
-                    }
-                    if (board[i, j] == 1)
-                    {
-                        Console.Write(" ");
-                    }
-                    if (board[i, j] == 0)
-                    {
-                        Console.Write("+");
-                    }
-                }
-                Console.WriteLine();
+                BinaryFormatter formatter = new BinaryFormatter();
+                formatter.Serialize(stream, a);
+                stream.Position = 0;
+                return (T)formatter.Deserialize(stream);
             }
         }
+    }
+
+    [Serializable]
+    public class Cell
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public bool IsStartPoint { get; set; }
+        public bool IsBlock { get; set; }
+        public List<Cell> Neighbors { get; set; } = new List<Cell>();
+        public Cell Prev { get; set; }
+        public int DistanceFromStart { get; set; } //10
+        public int EvristDistance { get; set; } //Count to end * 10
+        public int Sum { get; set; } // DistanceFromStart + EvristDistance
     }
 }
